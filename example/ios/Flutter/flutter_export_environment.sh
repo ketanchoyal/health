572bda60bda3218648f017045e177d15a160c870
Ketan Choyal <kchoyal@recipeunlimited.com>
2022-06-30 15:04:38 -0400

    intial commit with delete healthdata feature

---
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/ketanchoyal/fvm/versions/stable"
+export "FLUTTER_APPLICATION_PATH=/Users/ketanchoyal/Desktop/Programming.nosync/Flutter.nosync/Packages/flutter-plugins/packages/health/example"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=/Users/ketanchoyal/Desktop/Programming.nosync/Flutter.nosync/Packages/flutter-plugins/packages/health/example/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=4.0.0"
+export "FLUTTER_BUILD_NUMBER=4.0.0"
+export "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=/Users/ketanchoyal/Desktop/Programming.nosync/Flutter.nosync/Packages/flutter-plugins/packages/health/example/.dart_tool/package_config.json"
